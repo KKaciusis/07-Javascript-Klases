adb5a12d130be6810e8416e1ea5b0a1ce8cd9e14
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-06-29 21:47:28 +0300

    random butu skaiciai

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Klasės kintamieji:
 miestas - tekstas,
 pastatymo data - datos formatas,
 adresas - tekstas,
-laiptinių skaičius - masyvas, turintis tiek elementų, kiek yra laiptinių. Pvz: [1,2,3,4]
+laiptinių skaičius - masyvas, turintis tiek elementų, kiek yra laiptinių. Pvz: [1, 2, 3, 4]
 butų skaičius - masyvas, turintis tiek elementų, kiek butų yra laiptinėje.Pvz: [20, 22, 19, 13]
 bendras butų skaičius - suskaičiuojamas automatiškai. Apskaičiavimui susikurti statinį metodą.
 
--- script.js
+++ script.js
@@ -8,12 +8,52 @@ class Namas {
         this.laiptioniuS = laiptioniuS;
         this.butuS = butuS;
     }
+    info = () => {
+        return 'Namo miestas: ' + this.miestas + ',   Namo statybos data: ' + this.statybosData + ', laiptiniu sk: ' + this.laiptioniuS.length + ',   Butu skaicius: ' + this.butuS.length;
+    }
+    static info2 = (x) => {
+        let result = 0;
 
+        x.butuS.forEach(element => {
+            result += element;
+        });
+        return result;
+    }
+}
+class Butas {
+    constructor(numeris, kambariuS, gyventojuS){
+        this.numeris = numeris;
+        this.kambariuS = kambariuS;
+        this.gyventojuS = gyventojuS;
+    }
+    info3 = () =>{
+        return 'Buto numeris: ' + this.numeris + '   ' + this.kambariuS + '   Gryventoju skaicius: ' + this.gyventojuS;
+    }
+}
+function randomButuS (min, max) {
+    return Math.floor((Math.random() * (max - min + 1)) + min)
 }
 let namai = [];
-
+let butuS = [];
 for (let i = 0; i < 5; i++){
-    let miestas = "Shit-a-lot " + (i + 1);
-    namai.push (new Namas(miestas)) 
+    namai.push(
+        new Namas(
+            "Shit-a-lot " + (i + 1),
+            new Date(),
+            'adresas 15',
+            [2, 4, 5],
+            butuS
+        )
+    ); 
 }
+for (let j = 0; j < 3; j ++){
+    butuS.push(randomButuS(1, 222))
+}
+let butai = [];
+butai.push(new Butas(789, "Dvidesim du kambariai.", 45));
+
 console.log(namai);
+console.log(namai[4].info());
+console.log(Namas.info2(namai[2]));
+console.log(butai)
+console.log((butai[0].info3()));
