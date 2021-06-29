1e1cd0a301182aeec254affee66a067dbd7dc92a
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-06-29 17:44:03 +0300

    structure concept

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict"
+
+class Namas {
+    constructor(miestas, statybosData, adresas, laiptioniuS, butuS) {
+        this.miestas = miestas;
+        this.statybosData = statybosData;
+        this.adresas = adresas;
+        this.laiptioniuS = laiptioniuS;
+        this.butuS = butuS;
+    }
+
+}
+let namai = [];
+
+for (let i = 0; i < 5; i++){
+    let miestas = "Shit-a-lot " + (i + 1);
+    namai.push (new Namas(miestas)) 
+}
+console.log(namai);
